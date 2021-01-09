989cf08738ae6559f24eae51f4170471e25f24f0
Francis James Dela Cruz <56282809+francizjamez@users.noreply.github.com>
2021-01-10 00:06:17 +0800

    Update client.js

--- client.js
+++ client.js
@@ -2,5 +2,31 @@ const io = require("socket.io-client");
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin,  output: process.stdout });
 
-// write your code here
+rl.question('Enter your username:', (name) => {
 
+    console.log(`Welcome ${name}`);
+    
+    const socket = io('http://localhost:3000');
+    let eventName = "simple chat message";
+
+    let chat = function(){
+        rl.question("> ", (msg) =>{
+            console.log(`Sending message: ${msg}`);
+            socket.emit(eventName, `${name}: ${msg}`);
+            chat();
+        })
+    }
+
+    socket.on('connect',  () =>{
+        console.log("Connected to server");
+        chat();
+    });
+
+    socket.on('disconnect', () => {
+        console.log("Disconnected from server")
+    })
+
+    socket.on(eventName, msg => {
+        console.log(msg);
+    });
+});
